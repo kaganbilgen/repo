88a51ff2e61bda8808d84ca4f9f052d5011c4746
kaganbilgen <kaganbilgen@gmail.com>
2023-10-04 16:25:25 +0300

    Sidebar working

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,38 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from "react"
+import { NavLink} from "react-router-dom"
 import Hamburger from 'hamburger-react'
 
 export default function Sidebar() {
     const [isOpen, setOpen] = React.useState(false)
-
     return (
         <div className="sidebar-container">
             <div className="sidebar-hamburger">
                 <Hamburger toggled={isOpen} toggle={setOpen} />
             </div>
-            <nav className="sidebar-links">
-                <NavLink
+            <nav className={isOpen ? "sidebar-links-active" : "sidebar-links"}>
+                <NavLink 
+                    onClick={()=> setOpen(false)}
+                    id="prices"
+                    className="sidebar-nav-btn"
+                    to="/prices"
+                >
+                    Preise
+                </NavLink>
+                <NavLink 
+                    onClick={()=> setOpen(false)}
+                    id="contact"
+                    className="sidebar-nav-btn"
+                    to="/contact"
+                >
+                    Kontakt
+                </NavLink>
+                <NavLink 
+                    onClick={()=> setOpen(false)}
                     id="impressum"
                     className="sidebar-nav-btn"
                     to="/impressum"
                 >
                     Impressum
-                </NavLink>
-                <NavLink
-                    id="prices"
-                    className="sidebar-nav-btn"
-                    to="/prices"
-                >
-                    Prices
-                </NavLink>
-                <NavLink
-                    id="contact"
-                    className="sidebar-nav-btn"
-                    to="/contact"
-                >
-                    Contact
                 </NavLink>
             </nav>
         </div>
